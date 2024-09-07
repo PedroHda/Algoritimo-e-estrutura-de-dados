e340178292ade8ce7d985d031da7271dc9ff04ba
Pedro Domiciano <88150764+PedroHda@users.noreply.github.com>
2024-09-06 22:32:55 -0300

    Update Dados.java

--- Dados.java
+++ Dados.java
@@ -1,12 +1,30 @@
 public class Dados {
    
-        private String elem;
+        // private String elem;
         
-        void setAdiciona(String elementos) {
-            this.elem = elementos;
+        // void setAdiciona(String elementos) {
+        //     this.elem = elementos;
+        // }
+        // String getPegaElem(){
+        //     return this.elem;
+        // }
+        private int numero;
+        private String endereco;
+
+        public void setNumero(int Nome){
+            this.numero = Nome;
         }
-        String getPegaElem(){
-            return this.elem;
+        // public void setEndereco(String Endereco){
+        //     this.endereco = Endereco;
+        // }
+        // public String getNome(){
+        //     return this.nome;
+        // }
+        public int getnumero(){
+            return this.numero;
+        }
+        public String toString(){
+            return " " + this.numero;
         }
     
 }
