c496939e9b53deb842eee9f96546ebf0771def0e
Pedro Domiciano <88150764+PedroHda@users.noreply.github.com>
2024-09-06 22:32:31 -0300

    Update Vetor.java

--- Vetor.java
+++ Vetor.java
@@ -1,8 +1,21 @@
 public class Vetor {
-    private Dados[] dados= new Dados[100];
+    private Dados[] dados = new Dados[100];
     private int total = 0;
 
-    public void adiciona(Dados elem){
-        // implementação
+    public void adiciona(Dados elem) {
+        this.dados[this.total] = elem;
+        this.total++;
+    }
+
+    public String toString() {
+        String str = "[";
+        for (int i = 0; i < this.total -1; i++) {
+            if (this.dados[i] != null) {
+                str += this.dados[i] + ",";
+            }
+        }
+        str+= this.dados[this.total-1] + "]";
+        return str;
+        
     }
 }
